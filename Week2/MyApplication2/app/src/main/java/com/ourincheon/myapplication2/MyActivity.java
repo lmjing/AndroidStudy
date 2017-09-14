ced68768398c5fc5e977a15e294b688c87ef04f9
lmjing <lmjing1234@gmail.com>
2017-09-14 14:47:18 +0900

    setContentView 설명

--- Week2/MyApplication2/app/src/main/java/com/ourincheon/myapplication2/MyActivity.java
+++ Week2/MyApplication2/app/src/main/java/com/ourincheon/myapplication2/MyActivity.java
@@ -9,6 +9,7 @@ public class MyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //생성한 xml을 객체화시켜 메모리에 할당(참조) ( == 인플레이션 한다. )
         setContentView(R.layout.activity_my);
 
         TextView tv = (TextView)findViewById(R.id.myTextView);
