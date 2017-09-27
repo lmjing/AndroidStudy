828d13217025e09ea83e73893aa7da42ed28fb62
lmjing <lmjing1234@gmail.com>
2017-09-27 13:50:37 +0900

    layout 연습(코드로 짜기&xml로 짜기)

---
+++ Project/Week4/MyApplication234/app/src/main/java/com/ourincheon/myapplication234/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ourincheon.myapplication234;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //4.레이아웃 실습2: 코드로 구현 코딩 해봄 ㅎㅎㅎ다 날려 먹었음(참고)
+    }
+}
