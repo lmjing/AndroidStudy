ec26aa1b6c3139692ae95c4e253240d1d803f89c
lmjing <lmjing1234@gmail.com>
2017-09-28 14:11:12 +0900

    weight 강의

--- Project/Week4/MyApplication234/app/src/main/java/com/ourincheon/myapplication234/exerciseActivity1.java
+++ Project/Week4/MyApplication234/app/src/main/java/com/ourincheon/myapplication234/exerciseActivity1.java
@@ -3,11 +3,11 @@ package com.ourincheon.myapplication234;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class exerciseActivity extends AppCompatActivity {
+public class exerciseActivity1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_exercise);
+        setContentView(R.layout.activity_exercise1);
     }
 }
