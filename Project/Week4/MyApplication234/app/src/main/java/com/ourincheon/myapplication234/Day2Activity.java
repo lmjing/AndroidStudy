a88aadeaa20ff3b1b20726c130ff8889c870645d
lmjing <lmjing1234@gmail.com>
2017-09-28 13:36:04 +0900

    코드로 Layout 구성

--- Project/Week4/MyApplication234/app/src/main/java/com/ourincheon/myapplication234/Day2Activity.java
+++ Project/Week4/MyApplication234/app/src/main/java/com/ourincheon/myapplication234/Day2Activity.java
@@ -2,12 +2,43 @@ package com.ourincheon.myapplication234;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class Day2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_day2);
+//        setContentView(R.layout.activity_day2);
+
+        // context = 화면 = UI = Activity
+        LinearLayout lin = new LinearLayout(this); // this = Day2Activity.this
+
+        LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+        );
+        lin.setLayoutParams(lparams);
+        lin.setOrientation(LinearLayout.VERTICAL);
+
+        Button b1 = new Button(this);
+        Button b2 = new Button(this);
+        LinearLayout.LayoutParams btnParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+        );
+        b1.setLayoutParams(btnParams);
+        b2.setLayoutParams(btnParams);
+        b1.setText("Button");
+        b2.setText("Button 2");
+//        b1.setId(View.generateViewId());
+//        b2.setId(View.generateViewId());
+
+        lin.addView(b1);
+        lin.addView(b2);
+        setContentView(lin);
     }
 }
