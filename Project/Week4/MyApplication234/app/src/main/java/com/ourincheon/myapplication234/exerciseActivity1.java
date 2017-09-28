4706ea4a7067f4eb3ae0e6363738ab6e3c349a35
lmjing <lmjing1234@gmail.com>
2017-09-28 14:28:59 +0900

    frameLayout & toggle에 따라 imageView 변경되도록

--- Project/Week4/MyApplication234/app/src/main/java/com/ourincheon/myapplication234/exerciseActivity1.java
+++ Project/Week4/MyApplication234/app/src/main/java/com/ourincheon/myapplication234/exerciseActivity1.java
@@ -2,12 +2,31 @@ package com.ourincheon.myapplication234;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class exerciseActivity1 extends AppCompatActivity {
+
+    ImageView imageView1, imageView2;
+    boolean bToggleImage = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_exercise1);
+        setContentView(R.layout.activity_framelayout);
+
+        imageView1 = (ImageView)findViewById(R.id.imageView);
+        imageView2 = (ImageView)findViewById(R.id.imageView2);
+    }
+
+    public void onButtonClicked(View view) {
+        if (bToggleImage) {
+            imageView1.setVisibility(View.VISIBLE);
+            imageView2.setVisibility(View.INVISIBLE);
+        }else {
+            imageView1.setVisibility(View.INVISIBLE);
+            imageView2.setVisibility(View.VISIBLE);
+        }
+        bToggleImage = !bToggleImage;
     }
 }
