f2309999d0d03d50a8ae925bafc0a1523a4722ff
lmjing <lmjing1234@gmail.com>
2017-10-12 15:16:32 +0900

    Step 5까지

--- Project/Week2/ExerciseApplication/app/src/main/java/com/ourincheon/exerciseapplication/Exercise2_MyLayout/MyLayoutActivity.java
+++ Project/Week2/ExerciseApplication/app/src/main/java/com/ourincheon/exerciseapplication/Exercise2_MyLayout/MyLayoutActivity.java
@@ -3,6 +3,8 @@ package com.ourincheon.exerciseapplication.Exercise2_MyLayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import com.ourincheon.exerciseapplication.R;
 
@@ -11,6 +13,10 @@ public class MyLayoutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_layout);
+//        setContentView(R.layout.activity_my_layout);
+        Button myButton = new Button(this);
+        RelativeLayout myLayout = new RelativeLayout(this);
+        myLayout.addView(myButton);
+        setContentView(myLayout);
     }
 }
