aabc8e57d62a65a2bba0ce32389819f24e38258b
lmjing <lmjing1234@gmail.com>
2017-10-12 14:47:23 +0900

    step3까지

---
+++ Project/Week2/ExerciseApplication/app/src/main/java/com/ourincheon/exerciseapplication/Exercise2_MyLayout/MyLayoutActivity.java
@@ -0,0 +1,16 @@
+
+package com.ourincheon.exerciseapplication.Exercise2_MyLayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ourincheon.exerciseapplication.R;
+
+public class MyLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_layout);
+    }
+}
