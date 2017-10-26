cb9f0fc8f00dd7cdd9f4780eb8a259d635b49203
lmjing <lmjing1234@gmail.com>
2017-10-26 14:03:11 +0900

    onKeyDown에 옮김

--- Project/studyAndroid/app/src/main/java/com/ourincheon/studyandroid/Week7_Lab6/day1026_MyViewActivity.java
+++ Project/studyAndroid/app/src/main/java/com/ourincheon/studyandroid/Week7_Lab6/day1026_MyViewActivity.java
@@ -11,33 +11,30 @@ import android.view.View;
  */
 
 public class day1026_MyViewActivity extends AppCompatActivity {
+    day1026_MyView myView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final day1026_MyView myView = new day1026_MyView(this);
+
+        myView = new day1026_MyView(this);
         setContentView(myView);
         myView.setFocusable(true);
-        myView.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
-                //굳이 키보드가 떨어질때 실행할 필요가 없으므로 삭제해준다.
-//                int action = keyEvent.getAction();
-//                if ( action == keyEvent.ACTION_UP) {
-                    switch (keyCode) {
-                        case KeyEvent.KEYCODE_J:
-                            myView.setX(-20); break;
-                        case KeyEvent.KEYCODE_K:
-                            myView.setX(20); break;
-                        case KeyEvent.KEYCODE_M:
-                            myView.setY(-20); break;
-                        case KeyEvent.KEYCODE_N:
-                            myView.setY(20); break;
-                    }
-                    view.invalidate();
-                    return true;
-//                }
-//                return false;
-            }
-        });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_J:
+                myView.setX(-20); break;
+            case KeyEvent.KEYCODE_K:
+                myView.setX(20); break;
+            case KeyEvent.KEYCODE_M:
+                myView.setY(-20); break;
+            case KeyEvent.KEYCODE_N:
+                myView.setY(20); break;
+        }
+        myView.invalidate();
+        return true;
     }
 }
