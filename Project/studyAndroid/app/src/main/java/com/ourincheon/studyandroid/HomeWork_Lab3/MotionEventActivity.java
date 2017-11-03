c8ed074b69c0da206c055e62e0d31b06a91cb30d
lmjing <lmjing1234@gmail.com>
2017-11-03 18:45:39 +0900

    문제6 해결

---
+++ Project/studyAndroid/app/src/main/java/com/ourincheon/studyandroid/HomeWork_Lab3/MotionEventActivity.java
@@ -0,0 +1,16 @@
+package com.ourincheon.studyandroid.HomeWork_Lab3;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mijeong on 2017. 11. 3..
+ */
+
+public class MotionEventActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
