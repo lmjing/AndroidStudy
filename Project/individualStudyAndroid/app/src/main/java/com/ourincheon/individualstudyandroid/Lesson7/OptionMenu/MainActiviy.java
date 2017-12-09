2236c72497ec7eabb8eead9ee783fe99c7499a13
lmjing <lmjing1234@gmail.com>
2017-12-09 17:24:25 +0900

    강의7 - 실습2 옵션 메뉴2 - 코드만 활용

--- Project/individualStudyAndroid/app/src/main/java/com/ourincheon/individualstudyandroid/Lesson7/OptionMenu/MainActiviy.java
+++ Project/individualStudyAndroid/app/src/main/java/com/ourincheon/individualstudyandroid/Lesson7/OptionMenu/MainActiviy.java
@@ -15,6 +15,12 @@ import com.ourincheon.individualstudyandroid.R;
  */
 
 public class MainActiviy extends AppCompatActivity {
+
+    //실습2
+    static final int FIRST_MENU = 1;
+    static final int SECOND_MENU = 2;
+    static final int THIRD_MENU = 3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,21 +29,31 @@ public class MainActiviy extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.mymenu, menu);
+        //실습1 - 메뉴 따로 정의해서 생성하기
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.mymenu, menu);
+
+        //실습2 - 코드로 옵션 메뉴 생성하기
+        MenuItem item1 = menu.add(0, FIRST_MENU, Menu.NONE, "apple");
+        item1.setIcon(R.mipmap.ic_launcher);
+        menu.add(0, SECOND_MENU, Menu.NONE, "grape").setIcon(R.mipmap.ic_launcher);
+        menu.add(0, THIRD_MENU, Menu.NONE, "banna").setIcon(R.mipmap.ic_launcher);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.apple:
+//            case R.id.apple:
+            case FIRST_MENU:
                 Toast.makeText(getApplicationContext(), "사과 메뉴",Toast.LENGTH_SHORT).show();
                 return true;
-            case R.id.grape:
+//            case R.id.grape:
+            case SECOND_MENU:
                 Toast.makeText(getApplicationContext(), "포도 메뉴",Toast.LENGTH_SHORT).show();
                 return true;
-            case R.id.banna:
+//            case R.id.banna:
+            case THIRD_MENU:
                 Toast.makeText(getApplicationContext(), "바나나 메뉴",Toast.LENGTH_SHORT).show();
                 return true;
             default:
