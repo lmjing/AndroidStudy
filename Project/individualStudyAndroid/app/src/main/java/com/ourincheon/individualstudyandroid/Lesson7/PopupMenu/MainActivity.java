e937898e53e381ea2b373d70b08ca695f48d61b8
lmjing <lmjing1234@gmail.com>
2017-12-09 18:30:30 +0900

    강의7 - PopupMenu

---
+++ Project/individualStudyAndroid/app/src/main/java/com/ourincheon/individualstudyandroid/Lesson7/PopupMenu/MainActivity.java
@@ -0,0 +1,39 @@
+package com.ourincheon.individualstudyandroid.Lesson7.PopupMenu;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.ourincheon.individualstudyandroid.R;
+
+/**
+ * Created by mijeong on 2017. 12. 9..
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.popup_menu_main_activity);
+    }
+
+    public void onClick(View view) {
+        PopupMenu popup = new PopupMenu(this, view);
+        popup.getMenuInflater().inflate(R.menu.popup_mymenu, popup.getMenu());
+
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Toast.makeText(getApplicationContext(), "클릭한 팝업 메뉴 "+item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        popup.show();
+    }
+}
