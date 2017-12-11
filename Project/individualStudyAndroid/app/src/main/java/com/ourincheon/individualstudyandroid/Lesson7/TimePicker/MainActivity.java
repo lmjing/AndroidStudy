3ca3a5cc0892426dade2553601d656e885cf98f8
lmjing <lmjing1234@gmail.com>
2017-12-11 19:36:33 +0900

    강의7 - TimePicker(다른 코드 방식)

--- Project/individualStudyAndroid/app/src/main/java/com/ourincheon/individualstudyandroid/Lesson7/TimePicker/MainActivity.java
+++ Project/individualStudyAndroid/app/src/main/java/com/ourincheon/individualstudyandroid/Lesson7/TimePicker/MainActivity.java
@@ -16,28 +16,58 @@ import java.util.Locale;
  * Created by mijeong on 2017. 12. 11..
  */
 
-public class MainActivity extends AppCompatActivity {
+//public class MainActivity extends AppCompatActivity {
+//    TimePicker timePicker;
+//    TextView textView;
+//
+//    @Override
+//    protected void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.timepicker_main_activity);
+//
+//        timePicker = (TimePicker)findViewById(R.id.timePicker);
+//        textView = (TextView)findViewById(R.id.textView);
+//        Button button = (Button)findViewById(R.id.button);
+//
+//        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+//            @Override
+//            public void onTimeChanged(TimePicker timePicker, int hour, int minute) {
+//                textView.setText(String.format(Locale.KOREA, "%d:%d", hour, minute));
+//            }
+//        });
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                timePicker.setIs24HourView(!timePicker.is24HourView());
+//            }
+//        });
+//    }
+//}
+
+public class MainActivity extends AppCompatActivity implements TimePicker.OnTimeChangedListener {
+    TimePicker timePicker;
+    TextView textView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.timepicker_main_activity);
 
-        final TimePicker timePicker = (TimePicker)findViewById(R.id.timePicker);
-        final TextView textView = (TextView)findViewById(R.id.textView);
+        timePicker = (TimePicker)findViewById(R.id.timePicker);
+        textView = (TextView)findViewById(R.id.textView);
         Button button = (Button)findViewById(R.id.button);
 
-        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
-            @Override
-            public void onTimeChanged(TimePicker timePicker, int hour, int minute) {
-                textView.setText(String.format(Locale.KOREA, "%d:%d", hour, minute));
-            }
-        });
+        timePicker.setOnTimeChangedListener(this);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 timePicker.setIs24HourView(!timePicker.is24HourView());
             }
         });
+    }
+
+    @Override
+    public void onTimeChanged(TimePicker timePicker, int hour, int minute) {
+        textView.setText(String.format(Locale.KOREA, "%d:%d", hour, minute));
     }
 }
