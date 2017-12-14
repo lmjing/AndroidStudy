84a9144a07038be003cc61015514777d85113a07
lmjing <lmjing1234@gmail.com>
2017-12-14 19:33:47 +0900

    강의4) Layout 코드로만 짜기 - PPT

--- Project/individualStudyAndroid/app/src/main/java/com/ourincheon/individualstudyandroid/Lesson4_Layout/MainActivity.java
+++ Project/individualStudyAndroid/app/src/main/java/com/ourincheon/individualstudyandroid/Lesson4_Layout/MainActivity.java
@@ -3,6 +3,7 @@ package com.ourincheon.individualstudyandroid.Lesson4_Layout;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -19,25 +20,24 @@ public class MainActivity extends AppCompatActivity {
 
         LinearLayout linearLayout = new LinearLayout(this);
         linearLayout.setOrientation(LinearLayout.VERTICAL);
-        LinearLayout.LayoutParams layout_params = new LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT);
-        linearLayout.setLayoutParams(layout_params);
+        linearLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT
+        ));
 
         Button oneBtn = new Button(this);
+        oneBtn.setId(View.generateViewId());
         oneBtn.setText("첫번째 버튼");
-        oneBtn.setLayoutParams(new ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT));
 
         Button twoBtn = new Button(this);
+        twoBtn.setId(View.generateViewId());
         twoBtn.setText("두번째 버튼");
-        twoBtn.setLayoutParams(new ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT));
 
-        linearLayout.addView(oneBtn);
-        linearLayout.addView(twoBtn);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+
+        linearLayout.addView(oneBtn, params);
+        linearLayout.addView(twoBtn, params);
 
         setContentView(linearLayout);
     }
