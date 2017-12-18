6b98504518fcda1b4857a64417f6636270de876b
lmjing <lmjing1234@gmail.com>
2017-12-18 16:11:12 +0900

    강의5) customView

---
+++ Project/individualStudyAndroid/app/src/main/java/com/ourincheon/individualstudyandroid/Lesson6/CustomView/MainActivity.java
@@ -0,0 +1,19 @@
+package com.ourincheon.individualstudyandroid.Lesson6.CustomView;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ourincheon.individualstudyandroid.R;
+
+/**
+ * Created by mijeong on 2017. 12. 18..
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lesson6_custom_view);
+    }
+}
