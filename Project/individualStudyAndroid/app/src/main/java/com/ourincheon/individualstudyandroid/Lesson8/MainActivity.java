dfc4fb5bd37797b95f5b8a9e6d19a82e326c094d
lmjing <lmjing1234@gmail.com>
2017-12-18 22:33:10 +0900

    강의8) arc 그리기

--- Project/individualStudyAndroid/app/src/main/java/com/ourincheon/individualstudyandroid/Lesson8/MainActivity.java
+++ Project/individualStudyAndroid/app/src/main/java/com/ourincheon/individualstudyandroid/Lesson8/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.ourincheon.individualstudyandroid.Lesson8.View.Arc;
 import com.ourincheon.individualstudyandroid.Lesson8.View.ComplexShape;
 import com.ourincheon.individualstudyandroid.Lesson8.View.SimpleShape;
 
@@ -15,6 +16,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new ComplexShape(this));
+        setContentView(new Arc(this));
     }
 }
