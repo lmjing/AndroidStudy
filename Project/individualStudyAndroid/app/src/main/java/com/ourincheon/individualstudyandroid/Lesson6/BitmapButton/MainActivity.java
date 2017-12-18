7f6c3752a4314323fa8439b22cfe7d46aea9d185
lmjing <lmjing1234@gmail.com>
2017-12-18 16:41:01 +0900

    강의6) bitmapButton 만들기

---
+++ Project/individualStudyAndroid/app/src/main/java/com/ourincheon/individualstudyandroid/Lesson6/BitmapButton/MainActivity.java
@@ -0,0 +1,23 @@
+package com.ourincheon.individualstudyandroid.Lesson6.BitmapButton;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+import com.ourincheon.individualstudyandroid.R;
+
+/**
+ * Created by mijeong on 2017. 12. 18..
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lesson6_bitmap_button);
+    }
+}
